62ecac1ab579b982bf9950141aeaaf93a1b202c2
Cristian Pisco <cristian.pisco@outlook.com>
2022-01-15 23:46:28 -0500

    configurando proyecto, falta agregar la base de datos.

---
+++ README.md
@@ -0,0 +1,6 @@
+# Curso avanzado de PHP
+
+## Comandos para ejecutar el proyecto
+```
+docker-compose up -d --build
+```
---
+++ src/public/index.php
@@ -0,0 +1,144 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_starup_error', 1);
+error_reporting(E_ALL);
+
+require_once '../vendor/autoload.php';
+
+session_start();
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/..');
+$dotenv->load();
+
+use App\Middlewares\AuthenticationMiddleware;
+use Illuminate\Database\Capsule\Manager as Capsule;
+use Aura\Router\RouterContainer;
+use WoohooLabs\Harmony\Harmony;
+use WoohooLabs\Harmony\Middleware\DispatcherMiddleware;
+use Laminas\HttpHandlerRunner\Emitter\SapiEmitter;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+use Laminas\Diactoros\Response;
+use WoohooLabs\Harmony\Middleware\LaminasEmitterMiddleware;
+use \Franzl\Middleware\Whoops\WhoopsMiddleware;
+
+$container = new DI\Container();
+
+$capsule = new Capsule;
+$capsule->addConnection([
+    'driver'    => getenv('DB_DRIVER'),
+    'host'      => getenv('DB_HOST'),
+    'database'  => getenv('DB_NAME'),
+    'username'  => getenv('DB_USER'),
+    'password'  => getenv('DB_PASS'),
+    'charset'   => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix'    => '',
+    'port'      => getenv('DB_PORT')
+]);
+
+// Make this Capsule instance available globally via static methods... (optional)
+$capsule->setAsGlobal();
+// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
+$capsule->bootEloquent();
+
+$log = new Logger('app');
+$log->pushHandler(new StreamHandler(__DIR__ . '/../logs/app.log', Logger::WARNING));
+
+$request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
+    $_SERVER,
+    $_GET,
+    $_POST,
+    $_COOKIE,
+    $_FILES
+);
+$routerContainer = new RouterContainer();
+$map = $routerContainer->getMap();
+$map->get('index', '/', [
+    'App\Controllers\IndexController',
+    'indexAction'
+]);
+$map->get('contact', '/contact', [
+    'App\Controllers\ContactController',
+    'indexAction'
+]);
+$map->post('sendContact', '/contact/send', [
+    'App\Controllers\ContactController',
+    'sendAction'
+]);
+$map->get('indexJobs', '/admin/jobs', [
+    'App\Controllers\JobsController',
+    'indexAction'
+]);
+$map->get('deleteJobs', '/admin/jobs/{id}/delete', [
+    'App\Controllers\JobsController',
+    'deleteAction'
+]);
+$map->get('addJobs', '/admin/jobs/add', [
+    'App\Controllers\JobsController',
+    'getAddJobAction'
+]);
+$map->post('saveJobs', '/admin/jobs/add', [
+    \App\Controllers\JobsController::class,
+    'getAddJobAction'
+]);
+$map->get('addUser', '/admin/users/add', [
+    'App\Controllers\UsersController',
+    'getAddUser'
+]);
+$map->post('saveUser', '/admin/users/save', [
+    'App\Controllers\UsersController',
+    'postSaveUser'
+]);
+$map->get('loginForm', '/login', [
+    'App\Controllers\AuthController',
+    'getLogin'
+]);
+$map->get('logout', '/logout', [
+    'App\Controllers\AuthController',
+    'getLogout'
+]);
+$map->post('auth', '/auth', [
+    'App\Controllers\AuthController',
+    'postLogin'
+]);
+$map->get('admin', '/admin', [
+    'App\Controllers\AdminController',
+    'getIndex'
+]);
+$map->get('admin.profile.changePassword', '/admin/profile/changePassword', [
+    'App\Controllers\ProfileController',
+    'changePassword'
+]);
+$map->post('admin.profile.savePassword', '/admin/profile/savePassword', [
+    'App\Controllers\ProfileController',
+    'savePassword'
+]);
+
+
+$matcher = $routerContainer->getMatcher();
+$route = $matcher->match($request);
+
+try{
+    $harmony = new Harmony($request, new Response());
+    //->addMiddleware(new FastRouteMiddleware($router))
+    if (getenv('DEBUG') === "true") {
+      $harmony->addMiddleware(new WhoopsMiddleware());
+    }
+    $harmony
+      ->addMiddleware(new LaminasEmitterMiddleware(new SapiEmitter()))
+      ->addMiddleware(new Middlewares\AuraRouter($routerContainer))
+      ->addMiddleware(new DispatcherMiddleware($container, 'request-handler'))
+      ->addMiddleware(new AuthenticationMiddleware())
+      ->run();
+} catch (Exception $e) {
+    $log->error($e->getMessage());
+    $emitter = new SapiEmitter();
+    $emitter->emit(new Response\EmptyResponse(500));
+} catch (Error $e) {
+    $log->error($e->getMessage());
+    $emitter = new SapiEmitter();
+    $emitter->emit(new Response\EmptyResponse(500));
+}
+
